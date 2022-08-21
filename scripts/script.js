cfd620f89229b7d87a14d879a74d68b48f16ea9b
gandi durga venkata sujith sagar <42533678+sujithsagar217@users.noreply.github.com>
2022-08-21 13:26:54 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ setTimeout(() => {
   username=username[0].toUpperCase()+username.slice(1);
   partnername=partnername[0].toUpperCase()+partnername.slice(1);
 
-  var love_percentage = Math.floor((Math.random() * 100) + 1);
+  
   if (username==null || partnername==null) {
     ;
 
@@ -21,12 +21,13 @@ setTimeout(() => {
   else if ( (username.includes("sujith") && partnername.includes("var")) || (username.includes("var") && partnername.includes("sujith")) || (username.includes("var") && partnername.includes("chin")) || (username.includes("chin") && partnername.includes("var")) ){
     var para = document.getElementById("result");
     /*Believe*/
-    love_percentage = 101;
+    var love_percentage = 101;
     para.innerHTML +=username+' and '+partnername+', your Love Percentage is '+love_percentage;
     para.style.margin=0;
     para.fontSize="21px";
   }
   else {
+    var love_percentage = Math.floor((Math.random() * 100) + 1);
     var para = document.getElementById("result");
     para.innerHTML +=username+' and '+partnername+', your Love Percentage is '+love_percentage;
     para.style.margin=0;
